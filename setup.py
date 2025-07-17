5ee46ff1eea0019ccee95b756537b96476326f10
Utkarsh <Utkarshkarki97@gmail.com>
2025-07-17 11:47:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Utkarsh Karki",
+    author_email="utkarshkarki97@gmail.com",
+    packages=find_packages()
+)
