887624fe7fcf537b34003b3a39ba5dc94c27ddf2
Utkarsh <Utkarshkarki97@gmail.com>
2025-07-20 18:21:38 +0530

    Data Variables

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,20 @@
+import os
+from datetime import date
+from dotenv import load_dotenv
+
+DATABASE_NAME = "US_VISA"
+
+COLLECTION_NAME = "visa_data"
+
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+FILE_NAME: str = "usvisa.csv"
+MODEL_FILE_NAME = "model.pkl"
+
